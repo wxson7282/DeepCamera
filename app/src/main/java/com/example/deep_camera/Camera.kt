2b2611870dd56e9c0ebd1a9166167e578b32f565
wxson <wxson@126.com>
2025-04-24 10:30:55 +0800

    on building

--- app/src/main/java/com/example/deep_camera/Camera.kt
+++ app/src/main/java/com/example/deep_camera/Camera.kt
@@ -1,59 +1,25 @@
 package com.example.deep_camera
 
-import android.content.Context
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.annotation.OptIn
-import androidx.camera.core.CameraSelector
 import androidx.camera.lifecycle.ExperimentalCameraProviderConfiguration
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
-fun Camera(modifier: Modifier = Modifier) {
+fun Modifier.Camera() {
     // 在这里编写您的屏幕内容
     Log.i("Camera", "Start")
-    ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val (cameraPreviewRef, cameraPanelRef) = remember { createRefs() }
-        Box(modifier = Modifier.constrainAs(cameraPreviewRef) {
-            top.linkTo(parent.top)
-            bottom.linkTo(cameraPanelRef.top, margin = 80.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }) {
-            CameraPreview()
-        }
-        Box(modifier = Modifier.constrainAs(cameraPanelRef) {
-            bottom.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }) {
-            CameraPanel()
-        }
-    }
+    CameraPreview()
 }
 
 @OptIn(ExperimentalCameraProviderConfiguration::class)
@@ -73,7 +39,7 @@ private fun CameraPreview() {
                 )
                 scaleType = PreviewView.ScaleType.FIT_CENTER    // 视频帧位于中心，无拉伸变形
                 implementationMode = PreviewView.ImplementationMode.PERFORMANCE  // SurfaceView 模式
-                setBackgroundColor(Color.Black.toArgb())
+                setBackgroundColor(Color.Gray.toArgb())
             }.also { previewView ->
                 previewView.controller = cameraController
                 cameraController.bindToLifecycle(lifecycleOwner)
@@ -90,70 +56,3 @@ private fun CameraPreview() {
         Log.i("Camera", "Update")
     }
 }
-
-@Composable
-private fun CameraPanel() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(1.dp)
-            .border(1.dp, Color.Blue),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        // 列举全部镜头
-        val context = LocalContext.current
-        val cameraLensList = getCameraLensList(context)
-//        val cameraLensList = getCameraList()
-        val checkedStates = remember {
-            mutableStateListOf<Boolean>().apply {
-                repeat(cameraLensList.size) { add(false) }
-            }
-        }
-        cameraLensList.forEachIndexed { index, cameraSelector ->
-            Column {
-                Text(text = "镜头$index")
-                Checkbox(
-                    checked = checkedStates[index],
-                    onCheckedChange = { checkedStates[index] = it }
-                )
-            }
-        }
-    }
-}
-
-private fun getCameraLensList(context: Context): List<CameraSelector> {
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-    val cameraSelectors = mutableListOf<CameraSelector>()
-
-    try {
-        val cameraProvider = cameraProviderFuture.get()
-        // 获取所有符合条件的相机Info
-        val cameraInfoList = cameraProvider.availableCameraInfos.filter {
-            it.lensFacing == CameraSelector.LENS_FACING_BACK
-        }
-        // 为每个后置相机创建一个 CameraSelector
-        cameraInfoList.forEachIndexed { index, cameraInfo ->
-            val cameraSelector = CameraSelector.Builder()
-                .addCameraFilter { cameraInfos ->
-                    cameraInfos.filter { it == cameraInfo }
-                }
-                .build()
-            cameraSelectors.add(cameraSelector)
-        }
-    } catch (e: Exception) {
-        Log.e("CameraUtils", "Failed to get camera provider", e)
-    }
-
-    return cameraSelectors
-}
-
-
-/**
- * 获取相机列表 only for test
- */
-//private fun getCameraList(): List<CameraSelector> {
-//    return mutableListOf<CameraSelector>(
-//        CameraSelector.DEFAULT_BACK_CAMERA,
-//        CameraSelector.DEFAULT_FRONT_CAMERA
-//    )
-//}
