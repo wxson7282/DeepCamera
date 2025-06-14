d55593ebf9583284baf04716e9995e9755464a39
wxson <wxson@126.com>
2025-06-14 16:37:41 +0800

    on building

--- manual_camera/src/main/java/com/example/manual_camera/MainActivity.kt
+++ manual_camera/src/main/java/com/example/manual_camera/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.manual_camera
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,6 +25,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -28,7 +33,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.manual_camera.ui.theme.ManualCameraTheme
 
@@ -36,21 +40,58 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) !=
+            android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+
         enableEdgeToEdge()
         setContent {
             ManualCameraTheme {
-                Content()
+                Content(this)
+                // 显示权限授予对话框
+                if (showGrantedDialog.value) {
+                    GrantedDialog(
+                        onDismiss = { showGrantedDialog.value = false }
+                    )
+                }
+                // 显示权限拒绝对话框
+                if (showDeniedDialog.value) {
+                    DeniedDialog(
+                        onDismiss = {
+                            showDeniedDialog.value = false
+                            finish()    // 关闭应用
+                        }
+                    )
+                }
             }
+        }
+    }
+
+    private val showGrantedDialog = mutableStateOf(false)
+    private val showDeniedDialog = mutableStateOf(false)
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // 权限已授予，可执行相关操作
+            showGrantedDialog.value = true
+        } else {
+            // 权限被拒绝，可提示用户
+            showDeniedDialog.value = true
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun Content() {
+private fun Content(context: Context) {
+    val focusDistance = Util.getMinFocusDistance(context)
     var stateOfZoomRatio by remember { mutableFloatStateOf(0.5f) }
-    var stateOfFocusDistance by remember { mutableFloatStateOf(0.5f) }
-
+    var stateOfFocusDistance by remember { mutableFloatStateOf(focusDistance) }
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = { TopAppBar(title = { Text("Manual Camera Main") }) },
@@ -64,27 +105,20 @@ private fun Content() {
                     )
                 }
                 Column {
-                    Text(
-                        text = "Zoom Ratio: $stateOfZoomRatio",
-                    )
+                    Text(text = "Zoom Ratio: $stateOfZoomRatio")
                     Slider(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(2.dp)
                             .border(2.dp, MaterialTheme.colorScheme.primary),
                         value = stateOfZoomRatio,
                         onValueChange = { stateOfZoomRatio = it }
                     )
-                    Text(
-                        text = "Focus Distance: $stateOfFocusDistance",
-                    )
+                    Text(text = "Focus Distance: $stateOfFocusDistance")
                     Slider(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(2.dp)
                             .border(2.dp, MaterialTheme.colorScheme.primary),
                         value = stateOfFocusDistance,
-                        onValueChange = { stateOfFocusDistance = it }
+                        onValueChange = { stateOfFocusDistance = it },
+                        valueRange = 0f..focusDistance
                     )
                 }
             }
@@ -97,10 +131,31 @@ private fun Content() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ContentPreview() {
-    ManualCameraTheme {
-        Content()
-    }
+private fun GrantedDialog(onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("权限授予") },
+        text = { Text("相机权限已成功授予，可以使用相机功能。") },
+        confirmButton = {
+            Button(onClick = onDismiss) {
+                Text("确定")
+            }
+        }
+    )
 }
+
+@Composable
+private fun DeniedDialog(onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("权限拒绝") },
+        text = { Text("相机权限被拒绝，可能无法使用相机功能。") },
+        confirmButton = {
+            Button(onClick = onDismiss) {
+                Text("确定")
+            }
+        }
+    )
+}
+
--- manual_camera/src/main/java/com/example/manual_camera/Util.kt
+++ manual_camera/src/main/java/com/example/manual_camera/Util.kt
@@ -6,29 +6,32 @@ import android.hardware.camera2.CameraMetadata
 import android.util.Log
 import kotlin.collections.contains
 
-fun getMinFocusDistance(context: Context): Float {
-    val cameraManager = context.getSystemService(Context.CAMERA_SERVICE)
-            as android.hardware.camera2.CameraManager
-    val cameraId = cameraManager.cameraIdList.firstOrNull {
-        val characteristics = cameraManager.getCameraCharacteristics(it)
-        characteristics.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_BACK
+object Util {
+    fun getMinFocusDistance(context: Context): Float {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE)
+                as android.hardware.camera2.CameraManager
+        val cameraId = cameraManager.cameraIdList.firstOrNull {
+            val characteristics = cameraManager.getCameraCharacteristics(it)
+            characteristics.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_BACK
+        }
+        if (cameraId == null) {
+            Log.e("getMinFocusDistance", "No back camera found")
+            return 0f
+        }
+        val characteristic = cameraManager.getCameraCharacteristics(cameraId)
+        // 检查是否支持手动对焦
+        val afAvailability = characteristic.get(CameraCharacteristics.CONTROL_AF_AVAILABLE_MODES)
+        if (afAvailability == null || !afAvailability.contains(CameraMetadata.CONTROL_AF_MODE_OFF)) {
+            Log.e("getMinFocusDistance", "Camera does not support manual focus")
+            return 0f
+        }
+        // 获取最近焦距（最大值）
+        val minFocusDistance = characteristic.get(
+            CameraCharacteristics
+                .LENS_INFO_MINIMUM_FOCUS_DISTANCE
+        ) ?: 0f
+        Log.i("getMinFocusDistance", "minFocusDistance: $minFocusDistance")
+        return minFocusDistance
     }
-    if (cameraId == null) {
-        Log.e("getMinFocusDistance", "No back camera found")
-        return 0f
-    }
-    val characteristic = cameraManager.getCameraCharacteristics(cameraId)
-    // 检查是否支持手动对焦
-    val afAvailability = characteristic.get(CameraCharacteristics.CONTROL_AF_AVAILABLE_MODES)
-    if (afAvailability == null || !afAvailability.contains(CameraMetadata.CONTROL_AF_MODE_OFF)) {
-        Log.e("getMinFocusDistance", "Camera does not support manual focus")
-        return 0f
-    }
-    // 获取最近焦距（最大值）
-    val minFocusDistance = characteristic.get(
-        CameraCharacteristics
-            .LENS_INFO_MINIMUM_FOCUS_DISTANCE
-    ) ?: 0f
-    Log.i("getMinFocusDistance", "minFocusDistance: $minFocusDistance")
-    return minFocusDistance
 }
+
