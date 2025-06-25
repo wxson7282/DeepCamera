12b9e1b8443135c6c3c542b40dbc0aeca4cfe542
wxson <wxson@126.com>
2025-06-25 10:29:15 +0800

    on building

--- dual_camera/src/main/java/com/example/dual_camera/ConcurrentCameraScreen.kt
+++ dual_camera/src/main/java/com/example/dual_camera/ConcurrentCameraScreen.kt
@@ -24,31 +24,37 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
 
+@Suppress("DEPRECATION")
 @Composable
 fun ConcurrentCameraScreen() {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    // Create PreviewViews for both cameras
+    // 建立PreviewView
     val frontPreviewView = remember { PreviewView(context) }
     val backPreviewView = remember { PreviewView(context) }
-    // Initialize camera provider and cameras
+    // 初始化CameraProvider的监听器
     val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    // 初始化CameraProvider和ConcurrentCamera
     var cameraProvider by remember { mutableStateOf<ProcessCameraProvider?>(null) }
     var concurrentCamera by remember { mutableStateOf<ConcurrentCamera?>(null) }
 
     LaunchedEffect(cameraProviderFuture) {
-        // Wait for the camera provider to be available
+        // 监听CameraProvider的变化，从监听器中获取CameraProvider实例
         cameraProvider = cameraProviderFuture.get()
     }
 
     DisposableEffect(lifecycleOwner, cameraProvider) {
+        // 如果CameraProvider为空，则不执行任何操作
         val cameraProvider = cameraProvider ?: return@DisposableEffect onDispose {}
+        //定义前后摄像头的选择器
         val frontCameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
         val backCameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        // 创建前后摄像头的预览用例，并将其绑定到对应的PreviewView上
         val frontPreview = Preview.Builder().setTargetResolution(Size(640, 480)).build()
             .also { it.setSurfaceProvider(frontPreviewView.surfaceProvider) }
         val backPreview = Preview.Builder().setTargetResolution(Size(640, 480)).build()
             .also { it.setSurfaceProvider(backPreviewView.surfaceProvider) }
+        // 创建前后摄像头的配置
         val frontSingleCameraConfig = SingleCameraConfig(
             frontCameraSelector,
             UseCaseGroup.Builder().addUseCase(frontPreview).build(),
@@ -60,7 +66,9 @@ fun ConcurrentCameraScreen() {
             lifecycleOwner
         )
         try {
+            // 解除既有绑定
             cameraProvider.unbindAll()
+            // 绑定前后摄像头的配置，建立concurrentCamera
             cameraProvider.bindToLifecycle(
                 listOf(
                     frontSingleCameraConfig, backSingleCameraConfig
