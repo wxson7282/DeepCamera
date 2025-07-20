b2f7e3b5a6550be66081a392315a3520d3340c9f
wxson <wxson@126.com>
2025-07-20 11:00:38 +0800

    on building

--- dual_camera/src/main/java/com/example/dual_camera/Util.kt
+++ dual_camera/src/main/java/com/example/dual_camera/Util.kt
@@ -1,10 +1,12 @@
 package com.example.dual_camera
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
-import java.io.File
 import java.util.concurrent.Executors
 
 object Util {
@@ -17,17 +19,23 @@ object Util {
         val executor = Executors.newSingleThreadExecutor()
         // 如果是前置镜头，文件名前加上"—F"后缀，否则加上"—B"后缀
         val suffix = if (isFront) "-F" else "-B"
-        val photoFile = File(context.filesDir, "${System.currentTimeMillis()}$suffix.jpg")
-        val outputOptions =
-            ImageCapture.OutputFileOptions.Builder(photoFile)
-                .build()
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "${System.currentTimeMillis()}$suffix.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+        }
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+                context.contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            ).build()
         // 播放快门音效
         shutterSound?.play()
         imageCapture.takePicture(
             outputOptions, executor,
             object : ImageCapture.OnImageSavedCallback {
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    Log.i("takePictures", "Image saved to ${photoFile.absolutePath}")
+                    Log.i("takePictures", "Image saved to ${outputFileResults.savedUri}")
                 }
                 override fun onError(exception: ImageCaptureException) {
                     Log.e("takePictures", "Image capture failed", exception)
